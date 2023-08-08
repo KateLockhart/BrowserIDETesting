2fecc25c8af3e32a3dbc856642ee768fc3791b8a
KateLockhart <katelockhart707@gmail.com>
2023-08-08 15:01:07 -0400

    Added start of StackBlitz demo, fixed styling, general app maintenance/clean up.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import IDES from './browserIDEs';
--- src/browserIDEs.jsx
+++ src/browserIDEs.jsx
@@ -1,3 +1,17 @@
+/* const embedContent = (code) => {
+    return (
+        <pre>
+            {code}
+        </pre>
+    )
+}
+let exampleCode = `function foo(items) {
+    var i;
+    for (i = 0; i &lt; items.length; i++) {
+        alert("Ace Rocks " + items[i]);
+    }
+}` */
+
 let IDES = [
     {
         name: 'CodePen',
@@ -10,7 +24,8 @@ let IDES = [
         pros: [
             'Embedding via HTML, prefill embed, or iframe. No file explorer. ',
             'Can import packages and libraries via Skypack, React friendly. Will have to go into pen setting to turn on Babel to pack files for React/JSX rendering.',
-            'Set up for staff would mean a single Upright team to build pens for the embeds. Due to paid team/pro status, cost would be $1440/year for a team of 10.'
+            'Set up for staff would mean a single Upright team to build pens for the embeds. Due to paid team/pro status, cost would be $1440/year for a team of 10.',
+            'Team “pens” could be privatized for only organizational use in LMS.'
         ],
         cons: [
             'There is only what seems to be a paid tier organization system for teams with three user roles: Owner, Admin, Member. https://blog.codepen.io/documentation/team-management/ ',
@@ -30,6 +45,7 @@ let IDES = [
             'Has file explorer, ability to connect to GH for version control, and CLI command supported.',
             'Can embed via url or iFrame, embed functionality in beta though which may mean embed may not support full IDE features.',
             'Does have a Team/Organization structure that is $15 billed annually per team member per month. There are seemingly only two permission levels, a read or write status. The paid version will allow us to build/use an unlimited amount of private repos and sandboxes. https://codesandbox.io/docs/learn/teams/permissions ',
+            'Team sandboxes could be privatized for only organizational use in LMS.'
         ],
         cons: [
             `Embed may have performance issues since it's a beta feature.`,
@@ -52,6 +68,38 @@ let IDES = [
             'Cannot observe student process / code history',
             'Does not seem to have user / permission levels',
             'Seems to have event issues in trials / demos, could be troublesome for new developers trying to treat fiddle embed like VSC.'
+        ] 
+    },
+    {
+        name: 'Replit',
+        siteURL: 'https://replit.com/',
+        embed: () => {return(<iframe title='Replit embed' src="https://replit.com/@KateLockhart1/Hello-HTML-and-Styles?embed=true" width="100%" height="500" />)},
+        pros: [
+            'Does have CLI command support, file explorer, and package installation support.',
+            'Can connect to GH via version control setting in Replit.',
+            'Does have different tiers of permissioning for staff/team, but as we know it can be frustrating if not implemented correctly by staff.',
+            'Can see students full coding history character by character.',
+            'Easy to import replits to classrooms, etc. once they are made.'
+        ],
+        cons: [
+            'Can be embedded as an iframe, but can only view code, not edit or demo it in embed. Must “open in repl” and open in a new tab to do the prompt. This may not allow us to view their metric/history though unless it is from a classroom style team. Needs further exploration in my opinion. '
+        ] 
+    },
+    {
+        name: 'StackBlitz',
+        siteURL: 'https://stackblitz.com/',
+        embed: () => {return(<iframe title="StackBlitz embed" src='https://stackblitz.com/edit/stackblitz-starters-hmps8n?embed=1&file=src%2FApp.js&hideNavigation=1' width="100%" height="500" />)
+        },
+        pros: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        cons: [
+            '',
+            '',
+            ''
         ] 
     }
 ];
